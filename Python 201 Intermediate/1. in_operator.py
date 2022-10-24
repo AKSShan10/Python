c1d5fe826bc12cc3f00c04f5289093c86012676d
AKSShan10 <aks.shan6@gmail.com>
2022-10-24 19:38:16 +0600

    Python 201

---
+++ Python 201 Intermediate/1. in_operator.py
@@ -0,0 +1,27 @@
+my_answer = input("What is your answer? ")
+options = ["rock", "paper", "scissors"]
+
+if my_answer in options:
+    print("That option is a viable option")
+else:
+    print("Wrong answer!\ntry again")
+
+
+
+key = "name"
+person = {
+    "name": "Kalob",
+    "profession": "Coding teacher",
+}
+
+if key in person:
+    print("Name is a valid dictionary key in the person object")
+
+
+
+options = {"rock", "paper", "scissors"}
+
+if my_answer in options:
+    print("That option is a viable option")
+else:
+    print("Wrong answer!\ntry again")

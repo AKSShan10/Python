27157cccfb241d722d8a7d7a2164ce9c79f31bb1
AKSShan10 <aks.shan6@gmail.com>
2022-10-24 19:55:14 +0600

    Python 201

---
+++ Python 201 Intermediate/2. not_operator.py
@@ -0,0 +1,21 @@
+my_thing = True
+
+if my_thing == True:
+    print("Thing was true")
+else:
+    print("This is not true")
+
+
+#
+my_thing = False
+
+if not my_thing:
+    print("Print a statement here.")
+
+
+#
+name = "Kane Ezki"
+club_members = ['Kalob', 'Jon', 'Gully']
+
+if name not in club_members:
+    print("Kane is not part of the club")

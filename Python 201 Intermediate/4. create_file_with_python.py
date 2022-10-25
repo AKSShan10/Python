8c5363ed1fb5ead1fe839b1d6678c8f77080b1f3
AKSShan10 <aks.shan6@gmail.com>
2022-10-25 23:52:39 +0600

    Python 201

---
+++ Python 201 Intermediate/4. create_file_with_python.py
@@ -0,0 +1,8 @@
+with open('writing_file.txt','w') as file:
+    file.write("4. Hello from python 201! a second time")
+
+with open('4. writing_files.txt', 'w') as file:
+    file.write("")
+    file.write("\nA second line!")
+    file.write("\n\tThis is tabbed!")
+

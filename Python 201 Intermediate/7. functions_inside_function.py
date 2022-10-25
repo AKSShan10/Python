9e9a41634d4ad94005a84360eed6e9db97c24fd8
AKSShan10 <aks.shan6@gmail.com>
2022-10-26 00:44:39 +0600

    Python 201

---
+++ Python 201 Intermediate/7. functions_inside_function.py
@@ -0,0 +1,13 @@
+def thing1(name):
+
+    print("Welcome to thoing 1",name)
+    def thing2():
+        print('Welcome to thing 2',name)
+
+    thing2()
+    #DECORATOR
+
+thing1("X")
+
+#print(name) will show error.  
+# Scoping with functions?-->A variable is only available from inside the region it is created. This is called scope.

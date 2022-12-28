f02182efe6c696af59094c75c4c220c033ddd5e8
AKSShan10 <aks.shan6@gmail.com>
2022-12-28 20:17:06 +0600

    python 301

--- Python 301 Advanced Python/10. python_301_lesson_10_catching_exceptions.py
+++ Python 301 Advanced Python/10. python_301_lesson_10_catching_exceptions.py
@@ -1,8 +1,24 @@
-num = input('Emter a number: ')
+num1 = input('Emter a number: ')
+
+try:
+    num = int(num1)
+
+except ValueError:
+    print(num, 'was not a valid number.')
+
+except Exception as e:
+    print('Exception was caught')
+    print(type(e))
+    num = 'Unknown'
+
+print(num)
+
+
+num1 = input('Emter a number: ')
 num2 = input('Emter a second number: ')
 
 try:
-    num = int(num)
+    num = int(num1)
     num2 = int(num2)
     total = num/num2
 
